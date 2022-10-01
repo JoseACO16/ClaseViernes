423d78bf44bd24032b8ae5f27702cda7e01cd0f7
JoseCastillo16 <106782113+JoseCastillo16@users.noreply.github.com>
2022-09-30 20:49:40 -0600

    se creo ejercicio colas

---
+++ Estructuras_V/src/main/java/Semana_3/Queue.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Semana_3;
+
+/**
+ *
+ * @author XPC
+ */
+public class Queue<T> {
+    
+    //COLAS
+    
+    private node<T> head;
+    private node<T> tail;
+    
+    public void enqueue(T value){
+        node<T> newNode = new node<T>(value);
+        if(head == null){
+            head = newNode;
+            tail = newNode;
+        }
+        else{
+            tail.setNext(newNode);
+            tail = newNode;
+        }
+    }
+}
