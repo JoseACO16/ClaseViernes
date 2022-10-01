6ee4c957b3d7896380cb0e666712dbb2ba5efdd6
JoseCastillo16 <106782113+JoseCastillo16@users.noreply.github.com>
2022-09-30 19:26:40 -0600

    se creo la clase
    se creo la clase con sus atributos

---
+++ Estructuras_V/src/main/java/Semana_3/Stack.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Semana_3;
+
+/**
+ *
+ * @author XPC
+ */
+public class Stack<T> { 
+    
+     // nuestros atributos
+    
+    private node<T> top;
+    
+    
+    public void push(T value){
+        node<T> newNode = new node<T> (value);
+        if(top==null){
+            top=newNode;
+        }else{
+            newNode.setNext(top);
+            top=newNode;
+            
+        }
+        
+        
+    }
+    
+    public T pop(){
+        node<T> popped_value = top;
+        if(top!=null){
+            top=top.getNext();
+            return popped_value.getValue();
+        }else{
+            return null;
+        }
+        
+        
+    }
+}
---
+++ Estructuras_V/src/main/java/Semana_3/node.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Semana_3;
+
+/**
+ *
+ * @author XPC
+ */
+public class node<T> {
+    
+    private T value;
+    private node<T> next;
+    
+    public node(){
+        
+    }
+     
+    // quita el error del new
+    public node(T value){
+        this.value = value;
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    public void setValue(T value) {
+        this.value = value;
+    }
+
+    public node<T> getNext() {
+        return next;
+    }
+
+    public void setNext(node<T> next) {
+        this.next = next;
+    }
+}
