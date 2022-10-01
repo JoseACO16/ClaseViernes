42eeee2c5ea5af24ba43f6dfa39e81c78f0a5cbb
JoseCastillo16 <106782113+JoseCastillo16@users.noreply.github.com>
2022-09-30 19:27:00 -0600

    se realizo el ejercicio pilas

---
+++ Estructuras_V/src/main/java/Semana_3/main.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Semana_3;
+
+/**
+ *
+ * @author XPC
+ */
+public class main {
+    
+    public static void main(String[] args) {
+        Stack<Integer> Pila = new Stack<Integer>();
+        Pila.push(1);
+        Pila.push(2);
+        Pila.push(3);
+        System.out.println(Pila.pop());
+        System.out.println(Pila.pop());
+        System.out.println(Pila.pop());
+        System.out.println(Pila.pop());
+        
+        
+    }
+    
+}
--- Estructuras_V/src/main/java/com/mycompany/estructuras_v/Estructuras_V.java
+++ Estructuras_V/src/main/java/com/mycompany/estructuras_v/Estructuras_V.java
@@ -5,6 +5,7 @@
 
 package com.mycompany.estructuras_v;
 
+import Semana_3.Stack;
 import Tarea1.Ejercicio2;
 import java.util.Scanner;
 
@@ -17,14 +18,14 @@ public class Estructuras_V {
     public static void main(String[] args) {
 //        System.out.println("Hello World!");
 // ejercicio en clase y similar al ejercicio 3 tarea
-        System.out.println();
-        int[] arr ={12, 34, 64, 2, 3, };
-        //System.out.println("hola mundo");
-        //System.out.println("Hello World!");
-        //System.out.println(recursion.factorial(3));
-        //System.out.println(recursion.encontrarmax(arr, 1, arr[0]));
-        //fghftxhrfh
-        System.out.println(recursion.encontrarmax(arr, 1, arr[0]));
+//        System.out.println();
+//        int[] arr ={12, 34, 64, 2, 3, };
+//        //System.out.println("hola mundo");
+//        //System.out.println("Hello World!");
+//        //System.out.println(recursion.factorial(3));
+//        //System.out.println(recursion.encontrarmax(arr, 1, arr[0]));
+//        //fghftxhrfh
+//        System.out.println(recursion.encontrarmax(arr, 1, arr[0]));
         
         //ejercicio2
 //        Ejercicio2 p = new Ejercicio2();
@@ -32,5 +33,16 @@ public class Estructuras_V {
 //        Scanner sc = new Scanner(System.in);
 //        palabra = sc.next();
 //        System.out.println("String recusivamente: " + p.Ejercicio2(palabra));
+
+//main semana3
+        Stack<Integer> Pila = new Stack<Integer>();
+        Pila.push(1);
+        Pila.push(2);
+        Pila.push(3);
+        System.out.println(Pila.pop());
+        System.out.println(Pila.pop());
+        System.out.println(Pila.pop());
+        System.out.println(Pila.pop());
+
     }
 }
