f177a5327764ddeb1dfbdeb977d669961b75e68e
JoseCastillo16 <106782113+JoseCastillo16@users.noreply.github.com>
2022-10-11 21:00:34 -0600

    se hizo lo que pudo del ejercicio2

---
+++ Estructuras_V/src/main/java/Tarea2/Ejercicio2.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Tarea2;
+
+/**
+ *
+ * @author XPC
+ */
+public class Ejercicio2 {
+    
+}
